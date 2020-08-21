70baec82e59bcf0c9362bde4a23b9dcf3cac66dd
Chirag-Mathur <chiragmathurindia@gmail.com>
2020-08-21 19:02:28 +0530

    Hello World

---
+++ SquareRootDecomposition.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define MOD 1000000007
+int main()
+{
+     int t;
+     cin >> t;
+    cout<<"Hello World";
+    // while (t--)
+    // {
+    // }
+    return 0;
+}
